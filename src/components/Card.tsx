353dfcff12fa803b254d35ecd5e1070445154cca
Leonid <123672169+LBondarchuk@users.noreply.github.com>
2023-09-26 18:55:34 +0300

    'n'

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,7 @@ import { Button } from "./Button"
 export const Card = () => {
   return (
     <div className="card">
-      <img src="/images/card.jpeg" alt="img" className="card__img"/>
+      <img src="images/card.jpeg" alt="img" className="card__img"/>
       <div className="card__container">
         <h1 className="card__title">Title</h1>
         <div className="card__description">
